3c5ba79d4cbe44265e0c8f96010a2227acfc8d22
Alejandro9509 <alejandrobringas2021@gmail.com>
2022-04-14 01:09:56 -0700

    Markup some labels and tags

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 
 import './App.css';
-import {Container, Col, Row} from 'react-bootstrap'
+import {Container, Col, Row, Button} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [brk, setBrk] = useState(0)
+  const [session, setSession] = useState(0)
+
   return (
     <Container>
-      <Container></Container>
-      <Container>
-        <Row> 
-          <Col id="break-label"></Col>
-          <Col id="session-label"></Col>
-        </Row>
-      </Container>
-      <Container></Container>
+      <Row>
+        <Col>25 + 5 Clock</Col>
+      </Row>
+      <Row> 
+        <Col id="break-label">Break</Col>
+        <Col id="session-label">Session</Col>
+      </Row>
+      <Row>
+        <Col>
+          <Button id="break-decrement"></Button>
+          <Col id="break-length">5</Col>
+          <Button id="break-increment"></Button>
+        </Col>
+        <Col>
+          <Button id="session-decrement"></Button>
+          <Col id="session-length">25</Col>
+          <Button id="session-increment"></Button>
+        </Col>
+      </Row>
     </Container>
   );
 }
