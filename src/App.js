684714d1397d1de8f4c7be2232b1a78fb3600e30
Alejandro9509 <alejandrobringas2021@gmail.com>
2022-04-15 01:11:28 -0700

    Adding interval events

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@
 import './App.css';
 import {Container, Col, Row, Button} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
-  const [brk, setBrk] = useState(0)
-  const [session, setSession] = useState(0)
+  const [brk, setBrk] = useState(5)
+  const [session, setSession] = useState(25)
+  const [currentSession, setCurrentSession] = useState(25) 
+  const [currentSeconds, setCurrentSeconds] = useState(60)
+  const [isPlay, setIsPlay] = useState(true)
+
+  let interval; 
+ 
+  useEffect(() => {
+    if(isPlay){
+      const interval = setInterval(() => {
+        setCurrentSeconds(currentSeconds => currentSeconds -1)
+      }, 1000); 
+      return () => clearInterval(interval)
+    }
+
+  }, [isPlay])
+ 
 
   return (
     <Container>
@@ -20,14 +36,42 @@ function App() {
       </Row>
       <Row>
         <Col>
-          <Button id="break-decrement"></Button>
-          <Col id="break-length">5</Col>
-          <Button id="break-increment"></Button>
+          <Button onClick={() => setBrk(brk+1)} id="break-decrement"></Button>
+          <Col id="break-length">{brk}</Col>
+          <Button onClick={
+            () => {
+              setBrk((brk > 0 )? brk-1: brk)
+            }
+          } id="break-increment"></Button>
         </Col>
         <Col>
-          <Button id="session-decrement"></Button>
-          <Col id="session-length">25</Col>
-          <Button id="session-increment"></Button>
+          <Button onClick={() => {
+            
+            setSession(session => session+1)
+            
+          }} id="session-decrement"></Button>
+          <Col id="session-length">{session}</Col>
+          <Button onClick={() => {
+            setSession((session > 0) ? session -1: session)
+          }} id="session-increment"></Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col id="timer-laberl">
+          Session
+        </Col>
+        <Col id="time-left" dangerouslySetInnerHTML={{ __html: currentSession + ":" + ((currentSeconds === 60)? '00' : currentSeconds )}}>
+        </Col>
+      </Row>
+      <Row> 
+        <Col>
+          <Button onClick={() => setIsPlay(true)}> Iniciar </Button>
+          <Button onClick={() => setIsPlay(false)}> Pausa </Button>
+          <Button onClick={() => {
+            setIsPlay(false)
+            setCurrentSeconds(60)
+            setCurrentSession(session)
+          }}>Detener</Button>
         </Col>
       </Row>
     </Container>
